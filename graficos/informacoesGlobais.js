303295868575b48f2a47cce53d203fd60ef233d1
albino-neto <105427115+albino-neto@users.noreply.github.com>
2024-12-05 08:22:59 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,12 +6,12 @@ async function vizualizarInformacoesGlobais() {
     const pessoasJogando = (dados.total_pessoas_que_praticam_esportes_regularmente/ 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_semana_praticando_esportes)
-    //const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemJogando = ((pessoasJogando / pessoasNoMundo ) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasJogando} bilhões</span> estão jogando em algum esporte e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> Jogando.<br>Isso significa que aproximadamente <span>${porcentagemJogando}%</span> de pessoas que estão jogando.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasJogando} bilhões</span> estão jogando em algum esporte e passam em média <span>${horas} horas Jogando.<br>Isso significa que aproximadamente <span>${porcentagemJogando}%</span> de pessoas que estão jogando.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
